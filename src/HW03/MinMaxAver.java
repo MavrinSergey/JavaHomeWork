c535a053a4891e8e3923b551635c187a007ab53a
SergeyMavrin <mavrin_sergei@list.ru>
2023-02-01 23:01:26 +0400

    hw03

--- src/HW03/MinMaxAver.java
+++ src/HW03/MinMaxAver.java
@@ -1,10 +1,31 @@
 package HW03;
-// Залдан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+
+// Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
 public class MinMaxAver {
     public static void main(String[] args) {
-        double y = 10.0;
-        double x = y / 3;
-        System.out.println(x);
-        System.out.println(x);
+        ArrayList<Integer> array = RandomRandom(10, 10);
+        int min = Collections.min(array);
+        int max = Collections.max(array);
+        int sum = array.stream().mapToInt(i -> i).sum();
+        int average = sum / array.size();
+        System.out.println(array);
+        System.out.println(min);
+        System.out.println(max);
+        System.out.println(sum);
+        System.out.println(average);
+
+    }
+    public static ArrayList<Integer> RandomRandom(int size, int range) {
+        Random random = new Random();
+        ArrayList<Integer> array = new ArrayList<>();
+        for (int i = 0; i < size ; i++) {
+            array.add(random.nextInt(range));
+        }
+        return array;
     }
 }
--- src/HW03/RemoveEvenNumbers02.java
+++ src/HW03/RemoveEvenNumbers02.java
@@ -1,62 +1,28 @@
 package HW03;
 
-import java.util.Arrays;
-
+import java.util.ArrayList;
+import java.util.Random;
 // Пусть дан произвольный список целых цисел, удалить из него четный числа.
 public class RemoveEvenNumbers02 {
     public static void main(String[] args) {
-        int[] array = new int[] {2, 5, 1, 6, 4, 8, 3, 7, 9};
-        int[] resArr = MergeSort(array);
-        printArray(resArr);
-
+        ArrayList<Integer> array = RandomRandom(10, 10);
+        ArrayList<Integer> resArr = RemovingEvenNumbers(array);
+        System.out.println(array);
+        System.out.println(resArr);
     }
-    public static int[] MergeSort(int[] arr){
-        int[] temparrey1 = Arrays.copyOfRange(arr, 0, arr.length/2);
-        int[] temparrey2 = Arrays.copyOfRange(arr, arr.length/2, arr.length);
-        int[] resArr = new int[arr.length];
-        printArray(temparrey1);
-        printArray(temparrey2);
-        int j = 0;
-        int k = 0;
-        for (int i = 0; i < arr.length ; i++) {
-            if (temparrey1[j] < temparrey2[k]) {
-                resArr[i] = temparrey1[j];
-                if (j < temparrey1.length-1) {
-                    i++;
-                    j++;
-                }
-            } else {
-                resArr[i] = temparrey2[k];
-                if (k < temparrey2.length-1) {
-                    i++;
-                    k++;
-                }
-            }
-            if (j == temparrey1.length-1 || k == temparrey2.length-1) {
-                if (j == temparrey1.length-1) {
-                    System.arraycopy(temparrey2,
-                            k,
-                            resArr,
-                            i,
-                            temparrey2.length-1-k);
-//                    resArr = Arrays.copyOfRange(temparrey2, k, temparrey2.length);
-                } else {
-                    System.arraycopy(temparrey1,
-                            j,
-                            resArr,
-                            i,
-                            temparrey1.length-1-j);
-//                    resArr = Arrays.copyOfRange(temparrey1, j, temparrey1.length);
-                }
-            }
-        }
+
+    public static ArrayList<Integer> RemovingEvenNumbers(ArrayList<Integer> arr) {
+        ArrayList<Integer> resArr = new ArrayList<>();
+        arr.stream().filter(i -> i % 2 != 0).forEach(x -> resArr.add(x));
         return resArr;
     }
 
-    public static void printArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + ", ");
+    public static ArrayList<Integer> RandomRandom(int size, int range) {
+        Random random = new Random();
+        ArrayList<Integer> array = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            array.add(random.nextInt(range));
         }
-        System.out.println();
+        return array;
     }
 }
